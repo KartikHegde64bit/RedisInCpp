7400096888dcdf68961d9efe07e4e33ffb461cb9
KartikHegde64bit <kartikhegde062@gmail.com>
2024-10-24 00:41:46 +0530

    send Pong response to ping request

--- src/Server.cpp
+++ src/Server.cpp
@@ -34,9 +34,39 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
+	// KB_1.4
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_port = htons(6379);
+
+	// bind the socket to port 6379 ( default TCP socket port )
+	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+		std::cerr << "Failed to bind to port 6379\n";
+		return 1;
+	}
+
+	// max length of pending connections
+	int connection_backlog = 5;
+	// generally success returns 0;
+	if (listen(server_fd, connection_backlog) != 0) {
+		std::cerr << "listen failed\n";
+		return 1;
+	}
+
+	// for client address
+	struct sockaddr_in client_addr;
+	int client_addr_len = sizeof(client_addr);
+	
+	std::cout << "Waiting for a client to connect...\n";
+
+	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+	if(client_fd >=0 ) {
+		std::cout << "Client connected\n";
+	}
+	// sends the response to the client
+	send(client_fd, "+PONG\r\n", strlen("+PONG\r\n"), 0);
+
+	close(server_fd);
 	
 }
