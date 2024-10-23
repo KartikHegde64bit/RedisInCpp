2963111aba0de9274659523412a106db69ff9e5f
KartikHegde64bit <kartikhegde062@gmail.com>
2024-10-23 22:57:13 +0530

    remove buffering logging

--- src/Server.cpp
+++ src/Server.cpp
@@ -12,5 +12,8 @@
 // argc -> argument count
 // argv -> argument vector ( command line arguments )
 int main(int argc, char **argv) {
-
+	// Flush after every std::cout / std::cerr
+	// i.e useful for debugging, flush the ouput without buffering for real time logging
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
 }
