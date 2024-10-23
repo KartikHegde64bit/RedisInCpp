5b6e5cacd01c500f9ebf079e66cdc925f6083a92
KartikHegde64bit <kartikhegde062@gmail.com>
2024-10-23 22:57:55 +0530

    create a socket connection

--- src/Server.cpp
+++ src/Server.cpp
@@ -16,4 +16,14 @@ int main(int argc, char **argv) {
 	// i.e useful for debugging, flush the ouput without buffering for real time logging
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
+
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	std::cout << "Logs from your program will appear here!\n";
+
+	// this line creates a TCP(SOCK_STREAM) socket using IPv4(AF_INET) -> KB_1.1
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+		std::cerr << "Failed to create server socket\n";
+		return 1;
+	}
 }
