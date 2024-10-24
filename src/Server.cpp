6b5c59f098e01e25535a50766b737a6eb6207165
Kartik Hegde <71443719+KartikHegde64bit@users.noreply.github.com>
2024-10-24 17:34:12 +0530

    Update Server.cpp

--- src/Server.cpp
+++ src/Server.cpp
@@ -35,13 +35,15 @@ void connection_handler(int client_fd){
 int main(int argc, char **argv) {
 	// Flush after every std::cout / std::cerr
 	// i.e useful for debugging, flush the ouput without buffering for real time logging
+	// KB_1.0
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	std::cout << "Logs from your program will appear here!\n";
 
-	// this line creates a TCP(SOCK_STREAM) socket using IPv4(AF_INET) -> KB_1.1
+	// KB_1.1 
+	// this line creates a TCP(SOCK_STREAM) socket using IPv4(AF_INET)
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0) {
 		std::cerr << "Failed to create server socket\n";
