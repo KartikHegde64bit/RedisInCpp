856cb8628260e50177696ad82d28b64eb472675a
KartikHegde64bit <kartikhegde062@gmail.com>
2024-11-14 00:37:46 +0530

    Definitions update

--- src/RdbFileReader.hpp
+++ src/RdbFileReader.hpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include<unordered_map>
+#include<KeyValueDataStructure.hpp>
 
 class RdbFileReader{
     public:
@@ -9,5 +12,12 @@ class RdbFileReader{
         std::string generateRdbFileStream();
 
     private:
+        std::string magicString;
+        std::unordered_map<std::string, std::string>> metaDataMap;
+        int keyValueTableSize;
+        int keyExpireTableSize;
+
+        // the below data structure covers both key - value store and key-expiry store
+        KeyValueDataStructure::KeyValueDataStructure keyValueDataStructure;
 
 };
